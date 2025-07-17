c8a509ef52808af63a1469a5ad82b3a574059bad
santmantilla92@gmail.com <Santmantilla>
2025-07-17 09:39:37 -0500

    feat: :zap: Se agregan nuevas funciones y se mejora el rendimiento
    Funciones nuevas y mejor rendimiento

--- notaslistas.py
+++ notaslistas.py
@@ -2,7 +2,7 @@ import os
 alumnos = []
 def agregar_alumno():
     global alumnos
-    nombre = input("Ingrese el nombre del alumno: ")
+    nombre = input("Ingrese el nombre del alumno: ").strip().lower()
     alumnos.append([nombre,[],[],[]])
 def main_menu()-> int:
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -54,25 +54,34 @@ def pedir_nota(tipo):
             print("Por favor, ingrese un número válido.")
         
 def registro_parcial(alumno):
-    nota1 = pedir_nota('Parcial #1')
-    nota2 = pedir_nota('Parcial #2')
-    nota3 = pedir_nota('Parcial #3')
-    alumno[1].append(nota1)
-    alumno[1].append(nota2)
-    alumno[1].append(nota3)
+    if(len(alumno[1]) >= 3):
+        print('No puedes ingresar mas de 3 notas')
+        os.system('pause')
+    else:
+        nota = pedir_nota('Parcial')
+        alumno[1].append(nota)
 
 def registro_quiz(alumno):
-    nota = pedir_nota('Quiz')
-    alumno[2].append(nota)
+    if(len(alumno[2]) >= 4):
+        print('No puedes ingresar mas de 4 notas')
+        os.system('pause')
+    else:
+        nota = pedir_nota('Quiz')
+        alumno[2].append(nota)
+
 
 def registro_trabajo(alumno):
-    nota = pedir_nota('Trabajo')
-    alumno[3].append(nota)
+    if(len(alumno[3]) >= 2):
+        print('No puedes ingresar mas de 2 notas')
+        os.system('pause')
+    else:
+        nota = pedir_nota('Trabajo')
+        alumno[3].append(nota)
     
 def registro_notas():
-    nombre = input('Ingrese el nombre del alumno al que le registrara la nota: ')
+    nombre = input('Ingrese el nombre del alumno al que le registrara la nota: ').strip().lower()
     for i in alumnos:
-        if(i[0] == nombre):
+        if(i[0].lower() == nombre):
             while True:
                 opcion2 = main_menu_calificaciones()
                 match opcion2:
@@ -88,8 +97,26 @@ def registro_notas():
                         break
     else:
         print(f"El alumno '{nombre}' no está registrado.")
-        os.system('pause')                
+        os.system('pause')
 
+def notas_final():
+    nombre = input('Ingrese el nombre del alumno del que quiere ver la nota final: ').strip().lower()
+    for i in alumnos:
+        if(i[0] == nombre):
+            parciales = i[1]
+            quices = i[2]
+            trabajos = i[3]
+
+            if (len(parciales) != 3 or len(quices) != 4 or len(trabajos) != 2):
+                print('No estan todas las notas registradas.')
+            else:
+                todas = parciales + quices + trabajos
+                promedio = round(sum(todas) / len(todas),2)
+                print(f'La nota final de {nombre} es: {promedio}')
+            break
+    else:
+        print(f"El alumno '{nombre}' no está registrado.")
+        os.system('pause')
 
 if __name__ == "__main__":
     while True:
@@ -105,9 +132,9 @@ if __name__ == "__main__":
             print(alumnos)
             os.system('pause')
         elif opcion == 3:
-            pass
-        elif opcion == 4:
+            notas_final()
             os.system('pause')
+        elif opcion == 4:
             break
         else:
             print("Opción no válida. Intente de nuevo.")
